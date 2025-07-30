c2954d1e0ad6112fe71515b346e34916b1b2e84f
Blake Costner <72231264+BlakeACostner@users.noreply.github.com>
2025-07-30 14:45:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if ingredients_list:
 
 
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
         
 
 
